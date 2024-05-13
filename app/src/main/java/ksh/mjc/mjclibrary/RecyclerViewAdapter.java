5f9a04c68ea6cc498c207542afb2de81e331b250
kimsihyun0129 <rlatlgus0129@naver.com>
2024-05-13 22:35:38 +0900

    commit

---
+++ app/src/main/java/ksh/mjc/mjclibrary/RecyclerViewAdapter.java
@@ -0,0 +1,58 @@
+package ksh.mjc.mjclibrary;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Item> items;
+
+    public RecyclerViewAdapter(Context context, List<Item> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Item item = items.get(position);
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvStudyRoomName;
+        int[] viewNames = {R.id.v9,R.id.v10,R.id.v11,R.id.tv12,R.id.tv13,R.id.tv14,R.id.tv15,R.id.tv16,R.id.tv17,R.id.tv18,R.id.v19,R.id.tv20,R.id.v21};
+        View[] views = new View[viewNames.length];
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvStudyRoomName = itemView.findViewById(R.id.tvStudyRoomName);
+            for(int i=0;i<viewNames.length;i++){
+                views[i] = itemView.findViewById(viewNames[i]);
+            }
+        }
+
+        public void bind(Item item) {
+            tvStudyRoomName.setText(item.getStudyRoomName());
+        }
+    }
+}
--- app/src/main/java/ksh/mjc/mjclibrary/StudyRoomReservationActivity.java
+++ app/src/main/java/ksh/mjc/mjclibrary/StudyRoomReservationActivity.java
@@ -3,6 +3,8 @@ package ksh.mjc.mjclibrary;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -13,14 +15,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class StudyRoomReservationActivity extends AppCompatActivity {
 
     ImageButton ibBack,ibSelectDate;//뒤로가기 버튼, 날짜선택버튼
     TextView tvSelectDate;//날짜선택 텍스트뷰
-    ListView rvStudyRoom;//리사이클러뷰
+    RecyclerView rvStudyRoom;//리사이클러뷰
+    String[] studyRoomNames = {"창의학습공간 스터디룸1", "창의학습공간 스터디룸2", "창의학습공간 스터디룸3", "세미나룸", "스터디라운지 스터디룸1", "스터디라운지 스터디룸2", "스터디라운지 스터디룸3"};
     Calendar calendar = Calendar.getInstance();
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");//날짜 형식 지정
     String dateItems[] = new String[7];
@@ -43,8 +48,23 @@ public class StudyRoomReservationActivity extends AppCompatActivity {
             dateItems[i] = sdf.format(calendar.getTime());
             calendar.add(Calendar.DAY_OF_YEAR,1);
         }
-    }
 
+        //1. 리사이클러뷰에 표시할 데이터
+        List<Item> items = new ArrayList<Item>();
+        for(int i=0;i<studyRoomNames.length; i++){
+            items.add(new Item(studyRoomNames[i]));
+        }
+
+        //2. 어댑터 생성
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this,items);
+
+        //3. 리사이클러뷰를 어떻게 표시할 것인지 생성(리니어레이아웃)
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        rvStudyRoom.setLayoutManager(layoutManager);
+
+        //리사이클러뷰와 어댑터를 연결
+        rvStudyRoom.setAdapter(adapter);
+    }
 
     public void onSelectDateButtonClick(View v) { //예약날짜나 드롭다운아이콘 버튼 클릭했을 경우
         //다이얼로그 생성
