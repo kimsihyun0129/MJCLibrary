251675e4221a3b3b3918ee74204801b555e8bea1
ENKH123 <115855097+ENKH123@users.noreply.github.com>
2024-05-26 23:47:01 +0900

    메인화면

--- app/src/main/java/ksh/mjc/mjclibrary/navigate.java
+++ app/src/main/java/ksh/mjc/mjclibrary/navigate.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
 public class navigate extends AppCompatActivity {
 
     private Toolbar toolbar;
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +22,7 @@ public class navigate extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        navigationView = findViewById(R.id.navigationView);
 
     }
 }
